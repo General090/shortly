0f5415642ada14338350327801d29d75540ed30a
General090 <gospelkalu33@gmail.com>
2025-05-02 13:27:33 +0100

    shorly

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import Logo from '../assets/logo.svg'
+const Nav = () => {
+  return (
+    <div>
+        <div>
+            <img src={Logo} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Nav
